913b288e3ce96f4ec287038b309f09d675603425
Navnit Kumar Baranwal <navnit.baranwal@gdn-commerce.com>
2021-06-08 20:15:38 +0530

    chaingin

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -3,17 +3,18 @@
 def call(Map param, Boolean usePipeline = true) {
     def branch = param.get('branch') as String
     def url = param.get('url') as String
-
-pipeline {
-    stages {
-        stage('Git Checkout') {
-            steps {
-                gitCheckout(
-                    branch: "${branch}",
-                    url: "${url}"
-                )
+if (usePipeline) {
+    pipeline {
+        stages {
+            stage('Git Checkout') {
+                steps {
+                    gitCheckout(
+                        branch: "${branch}",
+                        url: "${url}"
+                    )
+                    }
                 }
-            }
-       }
+           }
+        }
     }
 }
