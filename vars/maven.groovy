57bcd6d9d7c89cae988d9c43df0fa25050b3ef48
Navnit Kumar Baranwal <navnit.baranwal@gdn-commerce.com>
2021-06-11 14:39:16 +0530

    adding maven

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -1,8 +1,8 @@
 def build() {
-    def mvnhome= "/Users/navnitkumarbaranwal/apache-maven-3.8.1/bin/mvn"
-    sh '"${'mvnhome'}" -B -DskipTests clean package'
+    //def mvnhome= "/Users/navnitkumarbaranwal/apache-maven-3.8.1/bin/mvn"
+    sh '/Users/navnitkumarbaranwal/apache-maven-3.8.1/bin/mvn -B -DskipTests clean package'
 }
 def test(){
-    def mvnhome= "/Users/navnitkumarbaranwal/apache-maven-3.8.1/bin/mvn"
-    sh '"${'mvnhome'}" test'
+    //def mvnhome= "/Users/navnitkumarbaranwal/apache-maven-3.8.1/bin/mvn"
+    sh '/Users/navnitkumarbaranwal/apache-maven-3.8.1/bin/mvn test'
 }
