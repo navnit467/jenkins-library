2e1a60db7c38290a0e8658f873153209c9b58db1
navnit467 <36435012+navnit467@users.noreply.github.com>
2021-06-11 22:24:28 +0530

    Update maven.groovy

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -1,6 +1,9 @@
 def build() {
     def mvnhome = "/Users/navnitkumarbaranwal/apache-maven-3.8.1/bin/mvn"
-    sh "${mvnhome} -B -DskipTests clean package"
+    def settings = params.getOrDefault('settings','settings.xml')
+    def DskipTest = params.getOrDefault('DsikpTest','false')
+    sh "${mvnhome} -B -DskipTests clean package --settings ${settings} -DskipTest ${DskipTest}"    
+    
 }
 def test(){
     def mvnhome = "/Users/navnitkumarbaranwal/apache-maven-3.8.1/bin/mvn"
